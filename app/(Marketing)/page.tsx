e47f4a5f82ec6445b5f2277d9101535ee1b55f75
Prabhat Kumar <imt_2022086@iiitm.ac.in>
2024-12-07 21:22:44 +0530

    convex(backend) and clerk(auth)

--- app/(Marketing)/page.tsx
+++ app/(Marketing)/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./_components/header";
 import Heros from "./_components/Heros";
+import Footer from "./_components/footer";
 
  const Marketingpage = () => {
   return ( 
@@ -8,7 +9,10 @@ import Heros from "./_components/Heros";
        md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
     <Header />
     <Heros />
-     </div>
+</div>
+     <Footer />
+    
+    
     </div>
    );
  }
